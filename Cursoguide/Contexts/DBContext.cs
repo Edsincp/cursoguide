9b5595ce24ccbb6aaee2a8ea3ec63a6ec72a2895
edsinn <edsonpereiracarvalho10@gmail.com>
2024-08-26 17:20:25 -0300

    Novas alterações feitas

---
+++ Cursoguide/Contexts/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft. EntityFrameworkCore;
+using Courseguide.Objects. Models. Entities;
+using Cursoseguide.Contexts.Builders;
+
+namespace CourseGuide.Contexts
+{
+
+public class AppDBContext : DbContext
+    {
+        // Mapeamento Relacional dos Objetos no Bando de Dados
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+        // Conjunto: Usuário
+        public DbSet<UserModel> Users { get; set; }
+        // Fluent API      
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Entidades de Usuário:
+            UserBuilder.Build(modelBuilder);
+        }
+    }
+}
